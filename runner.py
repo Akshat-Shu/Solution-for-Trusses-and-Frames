5869efb01a59fdf7bf5a1e71c717787d4a14383e
Akshat-Shu <shuklaakshat.217@gmail.com>
2024-10-03 20:48:20 +0530

    Restore Runner.py to run for Example 1

--- runner.py
+++ runner.py
@@ -1,9 +1,6 @@
-from Classes.forces import unknown_force, known_force
-from Classes.joint import joint
-from Classes.member import member
 from Classes.truss_problem import Truss
 
+from Examples.Example1.truss_members1 import members as members1
 
-
-truss = Truss(members)
+truss = Truss(members1)
 truss.generate_and_solve()
