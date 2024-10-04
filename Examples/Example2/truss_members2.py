60f2ed22fd24e6ecf29871e1bd01156482656b5c
Akshat-Shu <shuklaakshat.217@gmail.com>
2024-10-04 11:32:51 +0530

    Revert "building this was much more interesting than writing 21 members for a model"
    This reverts commit d47fc29a4262fc0af65db818fe4f9cafc0fb7012.

--- Examples/Example2/truss_members2.py
+++ Examples/Example2/truss_members2.py
@@ -6,80 +6,14 @@ memberAB = member(
     1, [
         joint('B', [], [
             known_force(0, -10, 4, 2)
-        ], 4, 2),
-        joint('A', [
-            unknown_force(0, 1, 0, 0, 'A')
-        ], [], 0, 0)
-    ],[], []
+        ], 4, 2)
+    ], [
+        unknown_force(1, 1, 0, 0, 'A')
+    ], []
 )
 #TODO: implement the second example (solving a truss)
 memberBC = member(
-    2, [
-        joint('C', [], [
-            known_force(0,-10,4,2)
-        ], 4, 2),
-        joint('B', [], [
-            known_force(0, -10, 0, 0)
-        ], 0, 0)
-    ], [], []
+    2, joint('C', [], [
+        known_force
+    ])
 )
-
-memberCD = member(
-    3, [
-        joint('C', [], [
-            known_force(0,-10,0,0)
-        ], 0, 0),
-        joint('D', [], [], 4, 2)
-    ], [], []
-)
-
-memberAL = member(
-    4, [
-        joint('A', [
-            unknown_force(0, 1, 0, 0, 'A')
-        ], [], 0, 0),
-        joint('L', [], [], 4, 0)
-    ], [], []
-)
-
-memberBL = member(
-    5, [
-        joint('B', [], [
-            known_force(0, -10, 0, 0)
-        ], 0, 0),
-        joint('L', [], [], 0, -2)
-    ], [], []
-)
-
-memberKL = member(
-    6, [
-        joint('K', [], [], 0, 0),
-        joint('L', [], [], -4, 0)
-    ], [], []
-)
-
-memberBK = member(
-    7, [
-        joint('B', [], [
-            known_force(0, -10, 0, 0)
-        ], 0, 0),
-        joint('K', [], [], 4, -2)
-    ], [], []
-)
-
-memberCK = member(
-    8, [
-        joint('C', [], [
-            known_force(0, -10, 0, 0)
-        ], 0, 0),
-        joint('K', [], [], 0, -4)
-    ], [], []
-)
-
-memberCJ = member(
-    9,
-)
-
-members = [
-    memberAB, memberBC,
-]
